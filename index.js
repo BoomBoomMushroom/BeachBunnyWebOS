97919a945b87635ddffc42272d1d0b3984140545
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2026-06-10 19:36:51 -0400

    Added my first app!

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let topBar = document.getElementById("topBar")
+
+
+// the clock
 let topBarTimeElement = document.getElementById("topBarTime")
 let topBarTimeInterval = null
 function startPreciseClock() {
@@ -9,23 +13,26 @@ function startPreciseClock() {
 
     topBarTimeInterval = setTimeout(startPreciseClock, delay)
 }
-
 startPreciseClock();
 
+// window elements
+let blockAppClickAfterDrag = false
 let allWindowElements = document.getElementsByClassName("window")
 for(let i=0; i<allWindowElements.length; i++){
-    makeElementDraggable( allWindowElements[i] )
+    makeElementDraggable( allWindowElements[i], false )
 }
-function makeElementDraggable(element){
+function makeElementDraggable(element, isApp){
     let initX = 0
     let initY = 0
     let currentX = 0
     let currentY = 0
 
+    let initStyleTop = ""
+    let initStyleLeft = ""
+
     let headerElements = element.getElementsByClassName("header")
     if(headerElements.length > 0){
         for(let i=0; i<headerElements.length; i++){
-            console.log(headerElements[i])
             headerElements[i].onmousedown = startDragging;
         }
     }
@@ -37,6 +44,9 @@ function makeElementDraggable(element){
 
     function startDragging(e){
         e.preventDefault()
+        initStyleTop = element.style.top
+        initStyleLeft = element.style.left
+
         initX = e.clientX;
         initY = e.clientY;
         document.onmouseup = stopDragging;
@@ -56,10 +66,16 @@ function makeElementDraggable(element){
      function stopDragging() {
         document.onmouseup = null;
         document.onmousemove = null;
+        
+        if(isApp == false){ return }
+        // didn't move
+        if(initStyleTop == element.style.top && initStyleLeft == element.style.left){ return }
+
+        blockAppClickAfterDrag = true
     }
 }
 
-
+// Close window buttons
 let allCloseWindowButtons = document.getElementsByClassName("closeWindow")
 for(let i=0; i<allCloseWindowButtons.length; i++){
     allCloseWindowButtons[i].addEventListener("click", ()=>{
@@ -73,4 +89,74 @@ function hideElement(element){
 }
 function revealElement(element){
     element.classList.remove("hidden")
+}
+
+
+// make apps selectable
+let lastTappedElement = null
+let selectedApp = null
+let lastTapTime = 0
+let largestZIndex = 1
+
+let allApps = document.getElementsByClassName("app")
+for(let i=0; i<allApps.length; i++){
+    makeElementDraggable(allApps[i], true)
+    allApps[i].addEventListener("click", ()=>{
+        tapApp(allApps[i])
+    })
+}
+
+let appFunctions = {
+    "openPlaylistBB": openPlaylistBB
+}
+
+function tapApp(element){
+    if(blockAppClickAfterDrag == true){
+        blockAppClickAfterDrag = false
+        return
+    }
+
+    let now = new Date().getTime();
+    let dt = now - lastTapTime
+
+    if(element == selectedApp){
+        selectElement(null)
+    }
+    else{
+        selectElement(element)
+    }
+
+    if(element == lastTappedElement && dt <= 500){
+        // todo: open the app
+        selectElement(null)
+
+        appFunctions[element.getAttribute("onOpenApp")]()
+        //console.log("open app")
+    }
+    
+    lastTapTime = now
+    lastTappedElement = element
+}
+function selectElement(element){
+    if(selectedApp != null){
+        selectedApp.classList.remove("selectedApp")
+    }
+    if(element != null){
+        element.classList.add("selectedApp")
+    }
+    selectedApp = element
+}
+function makeGreatestZIndex(element){
+    element.style.zIndex = largestZIndex
+    largestZIndex++;
+    topBar.style.zIndex = largestZIndex
+}
+
+
+// open app functions
+function openPlaylistBB(){
+    let windowElement = document.getElementById("playlistBB_window")
+
+    makeGreatestZIndex(windowElement) // move to the top
+    revealElement(windowElement)
 }
